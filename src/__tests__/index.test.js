2c6c2bf15559f1403902fa1af167b31b0af55837
Victor S. Borges <vborges@gmail.com>
2020-05-13 16:31:21 -0300

    Tests if a <caption> inside a <table> is considered a name

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,13 +1,25 @@
 import React from 'react'
-import {render, fireEvent} from '@testing-library/react'
-import Counter from '../'
+import {render} from '@testing-library/react'
 
-test('increments the count', () => {
-  const {getByText} = render(<Counter />)
-  const button = getByText('0')
-  fireEvent.click(button)
-  expect(button).toHaveTextContent('1')
-  fireEvent.click(button)
-  expect(button).toHaveTextContent('2')
-})
-
+test('doens\'t considers a <caption> as <table>\'s "name"', () => {
+  const { getByRole } = render(
+    <table>
+      <caption>Monthly savings</caption>
+      <thead>
+        <tr>
+          <th>Month</th>
+          <th>Savings</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>January</td>
+          <td>$100</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+  expect(getByRole('table')).toBeTruthy();
+  expect(getByRole('table', { name: 'Monthly savings' }))
+    .toBeTruthy();
+});
